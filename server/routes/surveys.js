19be01c5103d5fa8b6b4759248e6dcbc400123ce
Alexandra Zuniga <alex.zuniga@hotmail.ca>
2021-11-11 17:03:57 -0500

    added survey routes and crud operations

---
+++ server/routes/surveys.js
@@ -0,0 +1,114 @@
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the survey model
+let survey = require('../models/surveys');
+
+/* GET surveys List page. READ */
+router.get('/', (req, res, next) => {
+  // find all surveys in the surveys collection
+  survey.find( (err, surveys) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('surveys/list', {
+        title: 'surveys',
+        surveys: surveys
+      });
+    }
+  });
+  //  GET the survey details page in order to add a new survey
+router.get('/add', (req, res, next) => {
+  console.log("survey details page");
+  res.render('surveys/details', {
+    title: 'Add Survey',
+    surveys: ''
+  });
+
+});
+
+// POST process the survey details page and create a new survey - CREATE
+router.post('/add', (req, res, next) => {
+
+   let newSurvey = survey({
+    "Title": req.body.Title,
+    "Author": req.body.Author,
+    "Question1": req.body.Question1,
+    "Question2": req.body.Question2,
+    "Question3": req.body.Question3,
+   });
+   survey.create(newSurvey, (err, survey)=>{
+    if(err){
+        console.log(err);
+        res.end(err);
+    }
+    else{
+        //refresh the survey index page
+        res.redirect('/surveys');
+      }
+    });
+  });
+
+// GET the survey Details page in order to edit an existing survey
+router.get('/:id', (req, res, next) => {
+  let id = req.params.id;
+  survey.findById(id, (err, surveyToEdit) =>{
+    if(err)
+    {
+      console.log(err);
+      res.end(err);
+    }
+    else
+    {
+      res.render('surveys/details', {title: 'Edit Survey', surveys: surveyToEdit});
+    }
+  });
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/:id', (req, res, next) => {
+   let id = req.params.id;
+   let editedSurvey = survey({
+     "_id": id,
+     "Title": req.body.Title,
+     "Author": req.body.Author,
+     "Question1": req.body.Question1,
+     "Question2": req.body.Question2,
+     "Question3": req.body.Question3,
+   });
+   survey.updateOne({_id: id}, editedSurvey, (err)=>{
+     if(err){
+       console.log(err);
+       res.end(err);
+     }
+     else
+     {
+       res.redirect('/surveys');
+     }
+   });
+
+
+});
+
+// GET - process the delete by user id
+router.get('/delete/:id', (req, res, next) => {
+  let id = req.params.id;
+  survey.remove({_id:id}, (err)=>{
+    if(err){
+      console.log(err);
+      res.end(err);
+    }
+    else
+    {
+      res.redirect('/surveys');
+    }
+  });
+});
+
+});
+
+
+module.exports = router;
