aabd84da1442bc7c042d3502eec9b547fe9cb3cf
Alexandra Zuniga <alex.zuniga@hotmail.ca>
2021-11-10 21:57:28 -0500

    changed survey model to array for now

--- client/src/app/model/survey.model.ts
+++ client/src/app/model/survey.model.ts
@@ -4,7 +4,7 @@ export class Survey
     //creating public properties
     constructor(
         //the question mark means it is optional
-        public items?: "collections/list"
+        public items?: string[]
 
     ){}
 }
