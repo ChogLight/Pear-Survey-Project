ff2f3e3e5b55c2d17c9df045232ce44c322534ce
Alexandra Zuniga <alex.zuniga@hotmail.ca>
2021-11-10 21:26:24 -0500

    added survey details

---
+++ client/src/app/model/survey.model.ts
@@ -0,0 +1,10 @@
+import { ListenOptions } from "net";
+export class Survey
+{
+    //creating public properties
+    constructor(
+        //the question mark means it is optional
+        public items?: "collections/list"
+
+    ){}
+}
