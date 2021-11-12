1c109b3d81c71fbe1e28e5cc3cb02baea27eb1ab
Alexandra Zuniga <alex.zuniga@hotmail.ca>
2021-11-11 21:39:38 -0500

    adjusted home route and added to git ignore file to hide db connection string

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pear Survey Builder' });
+  res.render('home', { title: 'Pear Survey Builder' });
+});
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Pear Survey Builder' });
 });
 
 module.exports = router;
