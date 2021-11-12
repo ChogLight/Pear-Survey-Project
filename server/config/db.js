efdb7dd650b4f754e5519362725fe3532899712c
Alexandra Zuniga <alex.zuniga@hotmail.ca>
2021-11-11 21:41:42 -0500

    temporarily removed db file

--- server/config/db.js
+++
@@ -1,4 +0,0 @@
-//conntecting to database
-module.exports = {
-    "URI": "mongodb+srv://AlexandraZuniga:hUVYB9ThGXJQneRf@dbserver.bwtgq.mongodb.net/PearGroupProject?retryWrites=true&w=majority"
-  };
