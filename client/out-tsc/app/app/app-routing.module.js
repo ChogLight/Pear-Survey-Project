f0e93aed512b8b076599368bbf6f21a232c69c58
ChogLight <sebas8812@gmail.com>
2021-11-24 17:11:16 -0500

    fixed button Surveys in navbar showing even when not logged

---
+++ client/out-tsc/app/app/app-routing.module.js
@@ -0,0 +1,20 @@
+import { __decorate } from "tslib";
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SurveyComponent } from './pages/survey/survey.component';
+const routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'survey', component: SurveyComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
+let AppRoutingModule = class AppRoutingModule {
+};
+AppRoutingModule = __decorate([
+    NgModule({
+        imports: [RouterModule.forRoot(routes)],
+        exports: [RouterModule]
+    })
+], AppRoutingModule);
+export { AppRoutingModule };
+//# sourceMappingURL=app-routing.module.js.map
---
+++ client/out-tsc/app/app/app.module.js
@@ -0,0 +1,30 @@
+import { __decorate } from "tslib";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SurveyComponent } from './pages/survey/survey.component';
+let AppModule = class AppModule {
+};
+AppModule = __decorate([
+    NgModule({
+        declarations: [
+            AppComponent,
+            HeaderComponent,
+            FooterComponent,
+            HomeComponent,
+            SurveyComponent
+        ],
+        imports: [
+            BrowserModule,
+            AppRoutingModule
+        ],
+        providers: [],
+        bootstrap: [AppComponent]
+    })
+], AppModule);
+export { AppModule };
+//# sourceMappingURL=app.module.js.map
